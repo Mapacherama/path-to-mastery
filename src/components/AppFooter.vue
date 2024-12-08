23d88b62544b24a70a00205764b0b06b03aa121f
Miyamoto Musashi <48964493+Mapacherama@users.noreply.github.com>
2024-12-08 09:51:16 +0100

    Updating the footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,10 +1,12 @@
 <template>
   <v-footer color="grey-darken-3" app>
     <v-row justify="center">
-      <v-btn text class="mx-2 white--text" href="#home">Home</v-btn>
-      <v-btn text class="mx-2 white--text" href="#about">About Us</v-btn>
-      <v-btn text class="mx-2 white--text" href="#services">Services</v-btn>
-      <v-btn text class="mx-2 white--text" href="#contact">Contact</v-btn>
+      <v-btn text class="btn-black mx-2" href="#journey">Journey</v-btn>
+      <v-btn text class="btn-black mx-2" href="#philosophy">Philosophy</v-btn>
+      <v-btn text class="btn-black mx-2" href="#start-path"
+        >Start Your Path</v-btn
+      >
+      <v-btn text class="btn-black mx-2" href="#contact">Contact</v-btn>
     </v-row>
   </v-footer>
 </template>
